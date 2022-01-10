8a314384549f89d3839a0064558c90f436216942
jsy4869 <1457179890@qq.com>
2022-01-10 11:10:11 +0800

    github修改用户名后测试提交

--- src/Other/StringTest.java
+++ src/Other/StringTest.java
@@ -2,6 +2,7 @@ package Other;
 
 public class StringTest {
     public static void main(String[] args) {
-        System.out.println("aaaaa");
+        String a = "123";
+        System.out.println(a);
     }
 }
